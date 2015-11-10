b2e8988ff0244949ec71e921809485bd0eb06228
Jesus <jesusgonzaleznovez@gmail.com>
2015-11-10 13:14:27 +0100

    Borrado utils.js

--- app/utils/utils.js
+++
@@ -1,9 +0,0 @@
-require('shelljs/global');
-var execute = function(command){
-    exec(command, function(status, output) {
-        console.log(command);
-        console.log('Exit status:', status);
-    });
-};
-exports.execute = execute;
-
