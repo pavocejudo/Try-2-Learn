6f9840c9760d80ab3f329c7b233d82c7e239e24b
Jesus <jesusgonzaleznovez@gmail.com>
2015-11-13 16:08:35 +0100

    pruebecillas

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { title:'Prueba' });
 });
+
+router.get('/ps', function(req,res,next) {
+    require('shelljs/global');
+    var data = exec('ps', {}).output;
+    res.render('version', { stdout: data.toString() });
+
+});
+
 
 
 module.exports = router;
