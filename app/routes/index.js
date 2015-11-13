0e2d3f44b2d50631fd3b836ca82e9dc368cd1026
Jesus <jesusgonzaleznovez@gmail.com>
2015-11-13 10:01:37 +0100

    deleted some files

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,21 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-/* GET hello world */
-router.get('/readcode', function(req, res) {
-  res.setHeader("Content-Type", "text/html");
-  res.render('readcode', { title: 'Lector de código' });
-  res.end();
-});
-
-
-router.post('/readcode',function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.send('post ok');
-    res.end();
-});
 
 module.exports = router;
