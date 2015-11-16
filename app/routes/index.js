e4c364f85cee661cc418ee56a12c87e596c4b2ee
Jesus <jesusgonzaleznovez@gmail.com>
2015-11-16 11:26:58 +0100

    update views

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title:'Prueba' });
+  res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
 });
 
 router.get('/ps', function(req,res,next) {
