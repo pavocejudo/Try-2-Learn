f89e15f30172be72ac7291fe210fdbafc518822d
Jesus <jesusgonzaleznovez@gmail.com>
2015-11-17 13:02:31 +0100

    Trabajando un poco la interfaz y alguna funcionalidad

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var langs = ["Python2", "Python3", "Ruby"];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', { langs: langs });
 });
 router.post('/compiled',function(req, res, next) {
     require('shelljs/global');
     var code = req.body.description;
-    var path = 'test/hello.py',
-        fs = require('fs'),
-        buffer = new Buffer(req.body.description.toString());
+    var path = 'test/hello.py';
+    var buffer = new Buffer(req.body.description.toString());
+    var data = '', data2 = '';
     fs.open(path, 'w', function(err, fd) {
         if (err) {
             throw 'error opening file: ' + err;
@@ -18,17 +20,15 @@ router.post('/compiled',function(req, res, next) {
         fs.write(fd, buffer, 0, buffer.length, null, function(err) {
             if (err) throw 'error writing file: ' + err;
             fs.close(fd, function() {
-                console.log('hello.py written');
-                var data = exec('docker build -t home/ubuntu-python-hello test/', {}).output;
-                console.log('image built');
-                data = exec('docker run -t home/ubuntu-python-hello', {}).output;
+                data = exec('docker build -t home/ubuntu -f test/docker_python test/', {}).output;
+                data = exec('docker run -t home/ubuntu', {}).output;
                 console.log('image runs');
-                res.render('compiled', { data: data.toString(), code: code });
-                var data2 = exec('docker stop $(docker ps -a -q)', {}).output;    
+                res.render('compiled', { data: data.toString(), code: code, lang: langs[parseInt(req.body.language)] });
+                data2 = exec('docker stop $(docker ps -a -q)', {}).output;
             })
         });
     });
-    
+    data2 = exec('rm ' + path, {}).output;     
     
 });
 /* GET about page. */
