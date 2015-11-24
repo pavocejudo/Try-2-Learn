341c48ec7ad9d6a02a035debd7cbcca5694bbd9a
Jesús Ángel <jesusgonzaleznovez@gmail.com>
2015-11-24 16:14:59 +0100

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -5,7 +5,7 @@ var build = module.exports.build = function(lang){
 
 var run = module.exports.run = function(){
     //return exec('docker run -t home/ubuntu', {}).output;
-    return exec('apt-get install -y docker-engine', {}).output;
+    return exec('sudo apt-get install -y docker-engine', {}).output;
 }
 
 var stop = module.exports.stop = function(path){
